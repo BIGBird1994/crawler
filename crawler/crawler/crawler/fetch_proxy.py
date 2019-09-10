c7700b4e9109f75ad7b68619a5d04f3817216716
xiaotao <wcgxiaotao@163.com>
2019-09-10 18:56:25 +0800

    Update fetch_proxy.py

--- crawler/crawler/crawler/fetch_proxy.py
+++ crawler/crawler/crawler/fetch_proxy.py
@@ -6,8 +6,8 @@ import logging
 
 class FetchProxy(object):
 
-    proxy_api = 'http://webapi.http.zhimacangku.com/getip?num=10&type=1&pro=&city=0&yys=0&port=1&time=1&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions='
-    r = StrictRedis(host='35.221.151.226',port=6379,password="1x2yxtabc",db=0)
+    proxy_api = ''
+    r = StrictRedis(host='35.221.151.226',port=6379,password="123",db=0)
     
 
     def get_proxy(self):
